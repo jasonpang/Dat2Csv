c26426ded5a9a2de41daa2c0b798ef135ff257c3
Jason Pang <jasonpang2011@gmail.com>
2015-05-16 13:16:00 -0700

    Fixed GetBit tests to test full range of each type.

--- Dat2Csv.Tests/IntegerBitExtensionTests.cs
+++ Dat2Csv.Tests/IntegerBitExtensionTests.cs
@@ -12,15 +12,18 @@ namespace Dat2Csv.Tests
         {
             Assert.Throws<ArgumentOutOfRangeException>(() => ((byte)0).GetBit(-1));
             Assert.Throws<ArgumentOutOfRangeException>(() => ((byte)0).GetBit(sizeof(byte) * 8));
-            for (int i = 0; i < 7; i++, Assert.AreEqual(((byte)170).GetBit(i), i % 2 != 0)) ;
+            for (int i = 0; i < sizeof(byte) * 8; i++)
+                Assert.AreEqual(((byte)170).GetBit(i), i % 2 != 0);
 
             Assert.Throws<ArgumentOutOfRangeException>(() => ((short)0).GetBit(-1));
             Assert.Throws<ArgumentOutOfRangeException>(() => ((short)0).GetBit(sizeof(short) * 8));
-            for (int i = 0; i < 7; i++, Assert.AreEqual(((short)-21846).GetBit(i), i % 2 != 0)) ;
+            for (int i = 0; i < sizeof(short) * 8; i++)
+                Assert.AreEqual(((short)-21846).GetBit(i), i % 2 != 0);
 
             Assert.Throws<ArgumentOutOfRangeException>(() => ((ushort)0).GetBit(-1));
             Assert.Throws<ArgumentOutOfRangeException>(() => ((ushort)0).GetBit(sizeof(ushort) * 8));
-            for (int i = 0; i < 7; i++, Assert.AreEqual(((ushort)43690).GetBit(i), i % 2 != 0)) ;
+            for (int i = 0; i < sizeof(ushort) * 8; i++)
+                Assert.AreEqual(((ushort)43690).GetBit(i), i % 2 != 0);
         }
         [Test]
         public void TestSetBit()
