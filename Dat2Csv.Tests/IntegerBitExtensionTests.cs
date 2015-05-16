39ab46c4103bbb58e60c478b3996ba9983351f38
Jason Pang <jasonpang2011@gmail.com>
2015-05-16 12:51:23 -0700

    Added GetBit() impl and tests.

---
+++ Dat2Csv.Tests/IntegerBitExtensionTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Dat2Csv.Extensions;
+using NUnit.Framework;
+
+namespace Dat2Csv.Tests
+{
+    [TestFixture]
+    public class IntegerBitExtensionTests
+    {
+        [Test]
+        public void TestGetBit()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => ((byte)0).GetBit(-1));
+            Assert.Throws<ArgumentOutOfRangeException>(() => ((byte)0).GetBit(sizeof(byte) * 8));
+            for (int i = 0; i < 7; i++, Assert.AreEqual(((byte)170).GetBit(i), i % 2 != 0)) ;
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => ((short)0).GetBit(-1));
+            Assert.Throws<ArgumentOutOfRangeException>(() => ((short)0).GetBit(sizeof(short) * 8));
+            for (int i = 0; i < 7; i++, Assert.AreEqual(((short)-21846).GetBit(i), i % 2 != 0)) ;
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => ((ushort)0).GetBit(-1));
+            Assert.Throws<ArgumentOutOfRangeException>(() => ((ushort)0).GetBit(sizeof(ushort) * 8));
+            for (int i = 0; i < 7; i++, Assert.AreEqual(((ushort)43690).GetBit(i), i % 2 != 0)) ;
+        }
+    }
+}
---
+++ Dat2Csv/Extensions/IntegerBitExtensions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dat2Csv.Extensions
+{
+    /// <summary>
+    /// Provides methods to manipulate individual bits in primitive data types.
+    /// </summary>
+    public static class IntegerBitExtensions
+    {
+        /// <summary>
+        /// Returns the bit at position <see cref="bit"/>.
+        /// </summary>
+        /// <param name="number">The byte value.</param>
+        /// <param name="bit">The bit position to retrieve, where 0 is the LSB and 7 is the MSB.</param>
+        public static bool GetBit(this byte number, int bit)
+        {
+            if (bit < 0 || bit >= sizeof(byte) * 8)
+                throw new ArgumentOutOfRangeException("bit");
+            return (number & (1 << bit)) != 0;
+        }
+
+        /// <summary>
+        /// Returns the bit at position <see cref="bit"/>.
+        /// </summary>
+        /// <param name="number">The short value.</param>
+        /// <param name="bit">The bit position to retrieve, where 0 is the LSB and 7 is the MSB.</param>
+        public static bool GetBit(this short number, int bit)
+        {
+            if (bit < 0 || bit >= sizeof(short) * 8)
+                throw new ArgumentOutOfRangeException("bit");
+            return (number & (1 << bit)) != 0;
+        }
+
+        /// <summary>
+        /// Returns the bit at position <see cref="bit"/>.
+        /// </summary>
+        /// <param name="number">The unsigned short value.</param>
+        /// <param name="bit">The bit position to retrieve, where 0 is the LSB and 7 is the MSB.</param>
+        public static bool GetBit(this ushort number, int bit)
+        {
+            if (bit < 0 || bit >= sizeof(ushort) * 8)
+                throw new ArgumentOutOfRangeException("bit");
+            return (number & (1 << bit)) != 0;
+        }
+    }
+}
