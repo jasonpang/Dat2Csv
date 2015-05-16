1f043272924cbb673e795380f680515e37655825
Jason Pang <jasonpang2011@gmail.com>
2015-05-16 13:14:14 -0700

    Added SetBit() impl and tests.

--- Dat2Csv.Tests/IntegerBitExtensionTests.cs
+++ Dat2Csv.Tests/IntegerBitExtensionTests.cs
@@ -22,5 +22,29 @@ namespace Dat2Csv.Tests
             Assert.Throws<ArgumentOutOfRangeException>(() => ((ushort)0).GetBit(sizeof(ushort) * 8));
             for (int i = 0; i < 7; i++, Assert.AreEqual(((ushort)43690).GetBit(i), i % 2 != 0)) ;
         }
+        [Test]
+        public void TestSetBit()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => ((byte)0).SetBit(-1));
+            Assert.Throws<ArgumentOutOfRangeException>(() => ((byte)0).SetBit(sizeof(byte) * 8));
+            byte v170Test = 0, v170Sol = 170;
+            for (int i = 0; i < sizeof(byte) * 8; i++)
+                v170Test = v170Test.SetBit(i, i % 2 != 0);
+            Assert.AreEqual(v170Test, v170Sol);
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => ((short)0).SetBit(-1));
+            Assert.Throws<ArgumentOutOfRangeException>(() => ((short)0).SetBit(sizeof(short) * 8));
+            short vn21846Test = 0, vn21846Sol = -21846;
+            for (int i = 0; i < sizeof(short) * 8; i++)
+                vn21846Test = vn21846Test.SetBit(i, i % 2 != 0);
+            Assert.AreEqual(vn21846Test, vn21846Sol);
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => ((ushort)0).SetBit(-1));
+            Assert.Throws<ArgumentOutOfRangeException>(() => ((ushort)0).SetBit(sizeof(ushort) * 8));
+            ushort v43690Test = 0, v43690Sol = 43690;
+            for (int i = 0; i < sizeof(ushort) * 8; i++)
+                v43690Test = v43690Test.SetBit(i, i % 2 != 0);
+            Assert.AreEqual(v43690Test, v43690Sol);
+        }
     }
 }
--- Dat2Csv/Extensions/IntegerBitExtensions.cs
+++ Dat2Csv/Extensions/IntegerBitExtensions.cs
@@ -46,5 +46,50 @@ namespace Dat2Csv.Extensions
                 throw new ArgumentOutOfRangeException("bit");
             return (number & (1 << bit)) != 0;
         }
+
+        /// <summary>
+        /// Returns the number with the bit at position <see cref="bit"/> set to the specified value.
+        /// </summary>
+        /// <param name="number">The byte value.</param>
+        /// <param name="bit">The bit position to set, where 0 is the LSB and 7 is the MSB.</param>
+        /// <param name="value">The value to set the bit to. Defaults to true.</param>
+        public static byte SetBit(this byte number, int bit, bool value=true)
+        {
+            if (bit < 0 || bit >= sizeof(byte) * 8)
+                throw new ArgumentOutOfRangeException("bit");
+            return value ?
+                (byte)(number | (1 << bit)) :
+                (byte)(number & ~(1 << bit));
+        }
+
+        /// <summary>
+        /// Returns the number with the bit at position <see cref="bit"/> set to the specified value.
+        /// </summary>
+        /// <param name="number">The short value.</param>
+        /// <param name="bit">The bit position to set, where 0 is the LSB and 7 is the MSB.</param>
+        /// <param name="value">The value to set the bit to. Defaults to true.</param>
+        public static short SetBit(this short number, int bit, bool value = true)
+        {
+            if (bit < 0 || bit >= sizeof(short) * 8)
+                throw new ArgumentOutOfRangeException("bit");
+            return value ?
+                (short)(number | (short)(1 << bit)) :
+                (short)(number & ~(1 << bit));
+        }
+
+        /// <summary>
+        /// Returns the number with the bit at position <see cref="bit"/> set to the specified value.
+        /// </summary>
+        /// <param name="number">The unsigned short value.</param>
+        /// <param name="bit">The bit position to set, where 0 is the LSB and 7 is the MSB.</param>
+        /// <param name="value">The value to set the bit to. Defaults to true.</param>
+        public static ushort SetBit(this ushort number, int bit, bool value = true)
+        {
+            if (bit < 0 || bit >= sizeof(ushort) * 8)
+                throw new ArgumentOutOfRangeException("bit");
+            return value ?
+                (ushort)(number | (1 << bit)) :
+                (ushort)(number & ~(1 << bit));
+        }
     }
 }
