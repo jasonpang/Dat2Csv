e1f87a6ab92e5afe2ae034f7ad9c1a1cdd9c85fc
Jason Pang <jasonpang2011@gmail.com>
2015-05-16 15:25:40 -0700

    Added main program, added check for sign ext.

--- Dat2Csv/DatasetReader.cs
+++ Dat2Csv/DatasetReader.cs
@@ -34,55 +34,79 @@ namespace Dat2Csv
             byte[] x0y0 = this.ReadBytes(3);
             dataset.X[0] = dataset.X[0].PopulateBits(x0y0[0], sourceBit: 0, destinationBit: 4, count: 8);
             dataset.X[0] = dataset.X[0].PopulateBits(x0y0[1], sourceBit: 4, destinationBit: 0, count: 4);
-            dataset.X[0] = dataset.X[0].SignExtend();
+            // If negative, sign extend
+            if (dataset.X[0].GetBit(11))
+                dataset.X[0] = dataset.X[0].SignExtend();
             dataset.Y[0] = dataset.Y[0].PopulateBits(x0y0[1], sourceBit: 0, destinationBit: 8, count: 4);
             dataset.Y[0] = dataset.Y[0].PopulateBits(x0y0[2], sourceBit: 0, destinationBit: 0, count: 8);
-            dataset.Y[0] = dataset.Y[0].SignExtend();
+            // If negative, sign extend
+            if (dataset.Y[0].GetBit(11))
+                dataset.Y[0] = dataset.Y[0].SignExtend();
 
             // Read z0 and x1
             byte[] z0x1 = this.ReadBytes(3);
             dataset.Z[0] = dataset.Z[0].PopulateBits(z0x1[0], sourceBit: 0, destinationBit: 4, count: 8);
             dataset.Z[0] = dataset.Z[0].PopulateBits(z0x1[1], sourceBit: 4, destinationBit: 0, count: 4);
-            dataset.Z[0] = dataset.Z[0].SignExtend();
+            // If negative, sign extend
+            if (dataset.Z[0].GetBit(11))
+                dataset.Z[0] = dataset.Z[0].SignExtend();
             dataset.X[1] = dataset.X[1].PopulateBits(z0x1[1], sourceBit: 0, destinationBit: 8, count: 4);
             dataset.X[1] = dataset.X[1].PopulateBits(z0x1[2], sourceBit: 0, destinationBit: 0, count: 8);
-            dataset.X[1] = dataset.X[1].SignExtend();
+            // If negative, sign extend
+            if (dataset.X[1].GetBit(11))
+                dataset.X[1] = dataset.X[1].SignExtend();
 
             // Read y1 and z1
             byte[] y1z1 = this.ReadBytes(3);
             dataset.Y[1] = dataset.Y[1].PopulateBits(y1z1[0], sourceBit: 0, destinationBit: 4, count: 8);
             dataset.Y[1] = dataset.Y[1].PopulateBits(y1z1[1], sourceBit: 4, destinationBit: 0, count: 4);
-            dataset.Y[1] = dataset.Y[1].SignExtend();
+            // If negative, sign extend
+            if (dataset.Y[1].GetBit(11))
+                dataset.Y[1] = dataset.Y[1].SignExtend();
             dataset.Z[1] = dataset.Z[1].PopulateBits(y1z1[1], sourceBit: 0, destinationBit: 8, count: 4);
             dataset.Z[1] = dataset.Z[1].PopulateBits(y1z1[2], sourceBit: 0, destinationBit: 0, count: 8);
-            dataset.Z[1] = dataset.Z[1].SignExtend();
+            // If negative, sign extend
+            if (dataset.Z[1].GetBit(11))
+                dataset.Z[1] = dataset.Z[1].SignExtend();
 
             // Read x2 and y2
             byte[] x2y2 = this.ReadBytes(3);
             dataset.X[2] = dataset.X[2].PopulateBits(x2y2[0], sourceBit: 0, destinationBit: 4, count: 8);
             dataset.X[2] = dataset.X[2].PopulateBits(x2y2[1], sourceBit: 4, destinationBit: 0, count: 4);
-            dataset.X[2] = dataset.X[2].SignExtend();
+            // If negative, sign extend
+            if (dataset.X[2].GetBit(11))
+                dataset.X[2] = dataset.X[2].SignExtend();
             dataset.Y[2] = dataset.Y[2].PopulateBits(x2y2[1], sourceBit: 0, destinationBit: 8, count: 4);
             dataset.Y[2] = dataset.Y[2].PopulateBits(x2y2[2], sourceBit: 0, destinationBit: 0, count: 8);
-            dataset.Y[2] = dataset.Y[2].SignExtend();
+            // If negative, sign extend
+            if (dataset.Y[2].GetBit(11))
+                dataset.Y[2] = dataset.Y[2].SignExtend();
 
             // Read z2 and x3
             byte[] z2x3 = this.ReadBytes(3);
             dataset.Z[2] = dataset.Z[2].PopulateBits(z2x3[0], sourceBit: 0, destinationBit: 4, count: 8);
             dataset.Z[2] = dataset.Z[2].PopulateBits(z2x3[1], sourceBit: 4, destinationBit: 0, count: 4);
-            dataset.Z[2] = dataset.Z[2].SignExtend();
+            // If negative, sign extend
+            if (dataset.Z[2].GetBit(11))
+                dataset.Z[2] = dataset.Z[2].SignExtend();
             dataset.X[3] = dataset.X[3].PopulateBits(z2x3[1], sourceBit: 0, destinationBit: 8, count: 4);
             dataset.X[3] = dataset.X[3].PopulateBits(z2x3[2], sourceBit: 0, destinationBit: 0, count: 8);
-            dataset.X[3] = dataset.X[3].SignExtend();
+            // If negative, sign extend
+            if (dataset.X[3].GetBit(11))
+                dataset.X[3] = dataset.X[3].SignExtend();
 
             // Read y3 and z3
             byte[] y3z3 = this.ReadBytes(3);
             dataset.Y[3] = dataset.Y[3].PopulateBits(y3z3[0], sourceBit: 0, destinationBit: 4, count: 8);
             dataset.Y[3] = dataset.Y[3].PopulateBits(y3z3[1], sourceBit: 4, destinationBit: 0, count: 4);
-            dataset.Y[3] = dataset.Y[3].SignExtend();
+            // If negative, sign extend
+            if (dataset.Y[3].GetBit(11))
+                dataset.Y[3] = dataset.Y[3].SignExtend();
             dataset.Z[3] = dataset.Z[3].PopulateBits(y3z3[1], sourceBit: 0, destinationBit: 8, count: 4);
             dataset.Z[3] = dataset.Z[3].PopulateBits(y3z3[2], sourceBit: 0, destinationBit: 0, count: 8);
-            dataset.Z[3] = dataset.Z[3].SignExtend();
+            // If negative, sign extend
+            if (dataset.Z[3].GetBit(11))
+                dataset.Z[3] = dataset.Z[3].SignExtend();
 
             return dataset;
         }
--- Dat2Csv/Program.cs
+++ Dat2Csv/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,58 @@ namespace Dat2Csv
     {
         public static void Main(string[] args)
         {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Please use the program as follows: \n \t <program> <input-file>");
+                Environment.Exit(1);
+            }
+
+            var inputFilename = args[0];
+            var outputFilename = Path.GetFileNameWithoutExtension(inputFilename) + ".csv";
+
+            bool firstRead = true;
+            ushort lastSequenceNumber = 0;
+
+            try
+            {
+                using (var outputStream = new FileStream(outputFilename, FileMode.Create, FileAccess.Write))
+                using (var writer = new StreamWriter(outputStream))
+                {
+                    using (var inputStream = new FileStream(inputFilename, FileMode.Open, FileAccess.Read))
+                    using (var reader = new DatasetReader(inputStream))
+                    {
+                        writer.WriteLine("x,y,z");
+                        while (reader.BaseStream.Position < reader.BaseStream.Length)
+                        {
+                            var dataset = reader.ReadDataset();
+
+                            if (firstRead)
+                            {
+                                firstRead = false;
+                                lastSequenceNumber = dataset.SequenceNumber;
+                            }
+                            else
+                            {
+                                if (lastSequenceNumber + 1 != dataset.SequenceNumber)
+                                {
+                                    Console.WriteLine(String.Format("Failed: Last sequence number was {0} but current sequence number is {1}.", lastSequenceNumber, dataset.SequenceNumber));
+                                    Console.WriteLine("Exiting...");
+                                    Environment.Exit(1);
+                                }
+                                lastSequenceNumber = dataset.SequenceNumber;
+                            }
+                            for (int i = 0; i < dataset.X.Length; i++)
+                                writer.WriteLine(String.Format("{0},{1},{2}", dataset.X[i], dataset.Y[i], dataset.Z[i]));
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(String.Format("Error: {0}", e.ToString()));
+            }
+
+            Console.WriteLine("Program finished!");
         }
     }
 }
