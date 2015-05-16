32ce155ab923bf50243aa874422f21698b545998
Jason Pang <jasonpang2011@gmail.com>
2015-05-16 14:26:56 -0700

    Added Dataset class.

---
+++ Dat2Csv/Dataset.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dat2Csv
+{
+    /// <summary>
+    /// Represents one set of the binary file's data.
+    /// </summary>
+    public class Dataset
+    {
+        public ushort SequenceNumber { get; set; }
+        public ushort[] X { get; set; }
+        public ushort[] Y { get; set; }
+        public ushort[] Z { get; set; }
+
+        public Dataset()
+        {
+            X = new ushort[4];
+            Y = new ushort[4];
+            Z = new ushort[4];
+        }
+    }
+}
